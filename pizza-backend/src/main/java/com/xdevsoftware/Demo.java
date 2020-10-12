ee5981f057b7ebed5aaccc061294b82f11501c5c
Richard Fichtner <r.fichtner@xdev-software.de>
2020-10-12 20:53:30 +0200

    lambda expressions make everything so much better

--- pizza-backend/src/main/java/com/xdevsoftware/Demo.java
+++ pizza-backend/src/main/java/com/xdevsoftware/Demo.java
@@ -1,11 +1,14 @@
 package com.xdevsoftware;
 
+import java.time.LocalDate;
+
 public class Demo
 {
 	
 	public static void main(final String[] args)
 	{
 		System.out.println("Hello world");
+		new Thread(()-> System.out.println("Hello lambda")).start();
 	}
 	
 }
